3dd8004ebdf4b9e3a3665e78ab424eac9c4a6193
Nicolas Brunie <nibrunie@gmail.com>
2019-09-04 22:59:44 +0200

    Prototyping intersection graph and graph coloring

--- asmde/parser.py
+++ asmde/parser.py
@@ -252,19 +252,82 @@ class LiveRange:
     def update_stop(self, new_stop, dbg_object=None):
         if self.stop is None or new_stop > self.stop:
             self.stop = new_stop
-            self.dbg_object = dbg_object
+            self.stop_dbg_object = dbg_object
     def update_start(self, new_start, dbg_object=None):
         if self.start is None or new_start < self.start:
             self.start = new_start
-            self.dbg_object = dbg_object
+            self.start_dbg_object = dbg_object
 
     def __repr__(self):
         return "[{}; {}]".format(self.start, self.stop)
+
+    def intersect(self, liverange):
+        """ Check intersection between @p self range and @p liverange """
+        #if liverange_bound_compare_lt(self.stop, liverange.start) or liverange_bound_compare_gt(self.start, liverange.stop):
+        if self.stop <= liverange.start or self.start >= liverange.stop:
+            return False
+        return True
+
+    @staticmethod
+    def intersect_list(lr_list0, lr_list1):
+        """ test if any of the live range of list @p lr_list0
+            intersects any of the live range of list @p lr_list1 """
+        for liverange0 in lr_list0:
+            for liverange1 in lr_list1:
+                if liverange0.intersect(liverange1):
+                    return True
+        return False
+
 
 class RegisterPool:
     def __init__(self, size):
         pass
 
+def liverange_bound_compare_gt(lhs, rhs):
+    if isinstance(lhs, PostProgram):
+        # PostProgram > all
+        return True
+    elif isinstance(rhs, PostProgram):
+        return False
+    else:
+        return lhs < rhs
+
+def liverange_bound_compare_lt(lhs, rhs):
+    return liverange_bound_compare_gt(rhs, lhs)
+
+class PostProgram:
+    """" After program ends timestamp """
+    def __gt__(self, int_value):
+        """ PostProgram is equals to +infty, so is always greater than any integer """
+        assert isinstance(int_value, int)
+        return True
+
+    def __ge__(self, int_value):
+        assert isinstance(int_value, int)
+        return True
+
+    def __lt__(self, int_value):
+        """ PostProgram is equals to +infty, so is always greater than any integer """
+        assert isinstance(int_value, int)
+        return False
+
+    def __le__(self, int_value):
+        assert isinstance(int_value, int)
+        return False
+
+    def __repr__(self):
+        return "PostProgram"
+
+class LiveRangeMap(dict):
+    def declare_pre_defined_reg(self, reg):
+        """ declare a register which is alive before program starts """
+        if not reg in self:
+            self[reg] = [LiveRange()]
+        self[reg][-1].update_start(-1, DebugObject("before program starts"))
+    def declare_post_used_reg(self, reg):
+        if not reg in self:
+            self[reg] = [LiveRange()]
+        self[reg][-1].update_stop(PostProgram(), DebugObject("after program ends"))
 
 class RegisterAssignator:
     def __init__(self, arch):
@@ -273,10 +336,9 @@ class RegisterAssignator:
     def process_program(self, bundle_list):
         pass
 
-    def generate_liverange_map(self, bundle_list):
-        """ generate a dict key -> live_range mapping each variable
-            to its liverange """
-        liverange_map = {}
+    def generate_liverange_map(self, bundle_list, liverange_map):
+        """ generate a dict key -> list of disjoint live-ranges
+            mapping each variable to its liverange """
         for index, bundle in enumerate(bundle_list):
             print(index, bundle)
             for insn in bundle.insn_list:
@@ -284,66 +346,132 @@ class RegisterAssignator:
                 for reg in insn.use_list:
                     print(reg)
                     if not reg in liverange_map:
-                        liverange_map[reg] = LiveRange(stop=index, stop_dbg_object=insn.dbg_object)
-                    else:
-                        liverange_map[reg].update_stop(index, dbg_object=insn.dbg_object)
+                        liverange_map[reg] = [LiveRange()]
+                    # we update the last inserted LiveRange object in reg's list
+                    liverange_map[reg][-1].update_stop(index, dbg_object=insn.dbg_object)
                 print("defs in {}".format(insn.def_list))
                 for reg in insn.def_list:
                     print(reg)
                     if not reg in liverange_map:
-                        liverange_map[reg] = LiveRange(start=index, start_dbg_object=insn.dbg_object)
-                    else:
-                        liverange_map[reg].update_start(index, dbg_object=insn.dbg_object)
+                        liverange_map[reg] = []
+                    if not(len(liverange_map[reg]) and liverange_map[reg][-1].start == index): 
+                        # only register a liverange once per index value
+                        liverange_map[reg].append(LiveRange(start=index, start_dbg_object=insn.dbg_object))
         return liverange_map
 
     def check_liverange_map(self, liverange_map):
         error_count = 0
         for reg in liverange_map:
-            liverange = liverange_map[reg]
-            if liverange.start == None and liverange.stop != None:
-                print("value {} is used @ {} without being defined!".format(reg, liverange.stop_dbg_object))
-                error_count += 1
-            if liverange.stop == None and liverange.start != None:
-                print("value {} is defined @ {} without being used!".format(reg, liverange.start_dbg_object))
-                error_count += 1
+            for liverange in liverange_map[reg]:
+                if liverange.start == None and liverange.stop != None:
+                    if liverange.stop_dbg_object is None:
+                        print("value {} is used @ {} without being defined!".format(reg, liverange.stop_dbg_object))
+                    else:
+                        print("value {} is used line {} without being defined!".format(reg, liverange.stop_dbg_object.src_line))
+                    error_count += 1
+                if liverange.stop == None and liverange.start != None:
+                    if liverange.start_dbg_object is None:
+                        print("value {} is defined @ {} without being used!".format(reg, liverange.stop_dbg_object))
+                    else:
+                        print("value {} is defined line {} without being used!".format(reg, liverange.start_dbg_object.src_line))
+                    error_count += 1
         return error_count == 0
+
+    def create_intersection_map(self, liverange_map):
+        """ Build the graph of liverange intersection from the
+            liverange_map """
+        conflict_map = {}
+        for reg in liverange_map:
+            conflict_map[reg] = set()
+            for reg2 in liverange_map:
+                if reg2 != reg and LiveRange.intersect_list(liverange_map[reg], liverange_map[reg2]):
+                    conflict_map[reg].add(reg2)
+        return conflict_map
+
+    def color_graph(self, graph):
+        max_color_num = 0
+        max_degree = 0
+        max_degree_node = None
+
+        color_map = {}
+
+        # start by pre-assigning colors to corresponding physical registers
+        for reg in graph:
+            if isinstance(reg, ArchRegister):
+                color_map[reg] = reg.index
+
+        while len(color_map) != len(graph):
+            # looking for node with max degree
+            max_reg = max([node for node in graph if not node in color_map], key=(lambda reg: len(list(node for node in graph[reg] if not node in color_map))))
+            unavailable_color_list = sorted([color_map[neighbour] for neighbour in graph[max_reg] if neighbour in color_map])
+
+            new_color = min(set(range(64)).difference(set(unavailable_color_list)))
+
+            print("register {} has been assigned color {}".format(max_reg, new_color))
+            color_map[max_reg] = new_color
+
+        return color_map
+
+    def check_color_map(self, graph, color_map):
+        for reg in graph:
+            reg_color = color_map[reg]
+            for neighbour in graph[reg]:
+                if reg_color == color_map[neighbour]:
+                    print("color conflict for {}({}) vs {}({})".format(reg, reg_color, neighbour, color_map[neighbour]))
+                    return False
+        return True
+
 
 
 
 test_string = """\
-ld $r4 = $r2[$r12]
-add $r3 = $r2, $r1
+add $r4 = $r5, $r5
+ld $r4 = $r4[$r12]
 ;;
-add $r3 = $r2, $r1
+add R(add) = $r2, $r1
 ;;
 add $r3 = R(add), $r1
 ld $r4 = $r2[$r12]
 ;;
+add R(beta) = $r4, $r3
+;;
+add $r0 = R(beta), R(add)
+;;
 """
 
 if __name__ == "__main__":
-    print(lexer.RegisterLexem.match("$r12"))
-    print(lexer.RegisterLexem.match("$r12[$r2]"))
-    print(lexer.OperatorLexem.match("["))
-    print(lexer.OperatorLexem.match("]"))
-    print(lexer.BundleSeparatorLexem.match(";;"))
-    print(lexer.generate_line_lexems(";;"))
-
     arch = DummyArchitecture()
     asm_parser = AsmParser(arch)
 
+    print("parsing input program")
     for line_no, line in enumerate(test_string.split("\n")):
         lexem_list = lexer.generate_line_lexems(line)
         dbg_object = DebugObject(line_no)
-        print("lexem_list: ", lexem_list)
         asm_parser.parse_asm_line(lexem_list, dbg_object=dbg_object)
-
     print(asm_parser.program)
 
     print("Register Assignation")
     reg_assignator = RegisterAssignator(arch)
-    liverange_map = reg_assignator.generate_liverange_map(asm_parser.program)
+
+    empty_liverange_map = LiveRangeMap()
+    print("Declaring pre-defined registers")
+    for reg_index in [5, 1, 2, 12]:
+        reg = arch.get_unique_reg_object(reg_index, reg_class=Register.Std)
+        empty_liverange_map.declare_pre_defined_reg(reg)
+
+    liverange_map = reg_assignator.generate_liverange_map(asm_parser.program, empty_liverange_map)
+
+    print("Declaring post-used registers")
+    for reg_index in [0]:
+        reg = arch.get_unique_reg_object(reg_index, reg_class=Register.Std)
+        empty_liverange_map.declare_post_used_reg(reg)
     print(liverange_map)
 
+    print("Checking liveranges")
     print(reg_assignator.check_liverange_map(liverange_map))
+
+    print("Graph coloring")
+    intersection_map = reg_assignator.create_intersection_map(liverange_map)
+    intersection_graph = reg_assignator.color_graph(intersection_map)
+    print(reg_assignator.check_color_map(intersection_map, intersection_graph))
 
