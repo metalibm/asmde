7ed5a386fa5ae288b66a3e858cef6f45cbf9fac3
Nicolas Brunie <nibrunie@gmail.com>
2019-09-28 12:24:05 +0200

    Adding output file writting test in test_asmde

--- test_asmde.py
+++ test_asmde.py
@@ -9,11 +9,18 @@ def test_basic():
         "examples/test_dual_split_regs.S",
         "examples/test_immediate.S",
     ]
+    # testing all available examples
     for test in test_list:
         print("executing {}".format(test))
         test_ret = subprocess.check_call("python3 asmde/parser.py --input {}".format(test).split(" "))
         print("{} test_ret={}".format(test, test_ret))
         assert test_ret == 0
+
+    # testing writing output file
+    test_ret = subprocess.check_call("python3 asmde/parser.py --input {} --output {}".format(
+        "examples/test_basic_2.S",
+        "test_basic_2.regalloc.h").split(" "))
+    assert test_ret == 0
 
 
 if __name__ == "__main__":
