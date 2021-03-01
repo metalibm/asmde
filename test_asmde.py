7e477beb5b242c210125eccd5c6eb7b5d98e1e53
Nicolas Brunie <nbrunie@kalray.eu>
2021-03-01 21:21:50 +0100

    [WiP] introducing disabled parsing test

--- test_asmde.py
+++ test_asmde.py
@@ -23,6 +23,22 @@ def test_basic():
         "test_basic_2.regalloc.h").split(" "))
     assert test_ret == 0
 
+def test_trace_parsing():
+    test_list = [
+        ("tests/asm_trace_test.trc", "tests/asmde_trace_test-count.expected"),
+    ]
+
+    for test, golden in test_list:
+        print("executing {}".format(test))
+        test_ret = subprocess.check_call("python3 asmde/asm_stats.py --arch kv3 --input {} --mode trace --output /tmp/asm_test.count".format(test).split(" "))
+        print("{} test_ret={}".format(test, test_ret))
+        assert test_ret == 0
+        test_ret = subprocess.check_call("diff /tmp/asm_test.count {}".format(golden).split(" "))
+        print("{} test_ret={}".format(test, test_ret))
+        assert test_ret == 0
+
 
 if __name__ == "__main__":
     test_basic()
+    # broken because asmde module is not available in default PYTHONPATH
+    # test_trace_parsing()
